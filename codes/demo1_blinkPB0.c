cb07eb8ffa0c17f7b2f5dbd1c78c7935e0a37cf1
pangoro24 <p.gonzalez7027@gmail.com>
2020-03-10 12:15:32 -0500

    Added demo 0 - Blinking Led

---
+++ codes/demo1_blinkPB0.c
@@ -0,0 +1,42 @@
+*
+ * Demo1_Blink_PB0.c
+ *
+ * Created: 15/02/2020 14:16:12
+ * Author : Pablo Gonzalez Robles
+ */ 
+
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	DDRB = 0b00000011;          // Set PB0 to be output
+	
+    while (1) 
+    {
+		//Hard blinking
+		/*
+		PORTB = 0b00000011;    // Set PB0 HIGH
+		_delay_ms(1000);
+		PORTB = 0b00000000;    // set PB0 LOW
+		_delay_ms(1000);
+		*/
+		
+		//Bitwise blinking
+		
+		PORTB |=(1<< PB0);
+		_delay_ms(250);
+		PORTB &= ~(1 << PB0);
+		_delay_ms(250);
+		
+		
+		//Toggle led
+		/*
+		PORTB ^= (1 << PB0);
+		_delay_ms(100);
+		*/
+    }
+}
+
