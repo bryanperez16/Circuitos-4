81a7d1c365576eaa9a92b9681e15918551283305
Pablo <p.gonzalez7027@gmail.com>
2020-06-24 15:45:41 -0500

    added eeprom demo codes

---
+++ codes/atmega328p/demo7_eeprom_read.c
@@ -0,0 +1,65 @@
+/*
+ * Demo7_eepromRead.c
+ *
+ *
+ * Created: 23/06/2020 15:26:12
+ * Author : Pablo Gonzalez Robles
+ */
+
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>//eeprom management library inside avr-libc
+
+#define startLed	   PB0 //green led
+#define blinkingLed	   PB1 //green led
+
+
+uint8_t Read_byte(int idx)
+{
+	uint8_t stored_byte ;
+	stored_byte = eeprom_read_byte((uint8_t*) idx) ;
+	return stored_byte;
+}
+
+uint16_t Read_word(int idx)
+{
+	uint16_t stored_word;
+	stored_word = eeprom_read_word((uint16_t*) idx);
+	return stored_word;
+
+}
+
+uint8_t* Read_block(int starting_idx)
+{
+	uint8_t stored_string [10];
+	eeprom_read_block (( void *) stored_string , (const void*)starting_idx,10) ;
+	return stored_string;
+}
+
+float Read_float(int idx)
+{
+	float f;
+	f = eeprom_read_float((float*) idx) ;
+	return f;
+}
+
+int main(void)
+{
+	cli();
+	DDRB |= (1<<startLed) | (1<<blinkingLed);
+	PORTB |= (1 << startLed);
+	_delay_ms(1000);
+	PORTB ^= (1 << startLed);
+	uint8_t stored_byte = Read_byte(0x12);
+	if(stored_byte == 0xFF)
+	{
+		PORTB |= (1 << blinkingLed);
+		_delay_ms(2000);
+	}
+    while (1) 
+    {
+    }
+}
+
---
+++ codes/atmega328p/demo7_eeprom_write.c
@@ -0,0 +1,87 @@
+/*
+ * Demo7_eepromWrite.c
+ *
+ * Created: 23/06/2020 10:15:54
+ * Author : Pablo Gonzalez Robles
+ */ 
+
+#define F_CPU 1000000UL 
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>//eeprom management library inside avr-libc
+
+
+
+#define blinkingLed	   PB0 //orange led
+#define defined_address 0x12c //random register . Register 300
+uint8_t EEMEM eeprom_var1; //memory char or byte variable
+uint8_t EEMEM own_string[10] ;
+
+
+void alert()
+{
+	PORTB ^= (1 << blinkingLed);
+	_delay_ms(2000);
+}
+
+void Write_byte_simple(int idx, int value)
+{
+	uint8_t byte2write ;
+	byte2write = value ;
+	eeprom_update_byte (( uint8_t *) idx , byte2write );
+	alert();
+}
+
+void Write_word_simple (int idx, int value)
+{
+	uint16_t word2write ;
+	word2write = value ; //0xabcd saved as 1st register = cd, 2nd register = ab 
+	eeprom_update_word (( uint16_t *) idx , word2write ) ;
+	alert();
+}
+
+
+void Write_float_simple(int idx, float value)
+{
+	eeprom_write_float(( float*) idx,value);
+}
+
+void Write_EEMEM()
+{
+	char tmp[] = "p#B1o,!";
+	eeprom_write_block(tmp, own_string, sizeof(tmp));
+	eeprom_write_byte(&eeprom_var1,0xbb); //1 byte
+	alert();
+}
+
+
+void Write_byte_properly(int idx, int value)
+{
+	uint8_t byte2write ;
+	byte2write = value ;
+	eeprom_update_byte (( uint8_t *) idx , byte2write);
+	alert();
+}
+
+
+int main(void)
+{
+	cli();
+	DDRB |= (1<<blinkingLed);   
+	Write_word_simple(30, 0x6543);
+	Write_float_simple(20,3.141592); 
+	Write_byte_simple(40,0x12);
+	Write_EEMEM();
+	Write_byte_properly(41,0x24);
+
+
+    while (1) 
+    {
+    }
+}
+
+
+
+
+
